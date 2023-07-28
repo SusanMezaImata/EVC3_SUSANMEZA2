a3049ee60821da87bdb2c18a68385720d09db3dc
Sujo <sm76560225@idat.edu.pe>
2023-07-28 02:43:25 -0500

    Initial commit

---
+++ app/src/main/java/pe/idat/ec3/susan/evc3_susanmeza/MainActivity.java
@@ -0,0 +1,14 @@
+package pe.idat.ec3.susan.evc3_susanmeza;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
